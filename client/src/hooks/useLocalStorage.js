46046f25e4d486a881dc893a674fcd33de7dbe57
thedevyansh <schawla333333@gmail.com>
2021-04-28 15:20:08 +0530

    modularized hook

--- client/src/hooks/useLocalStorage.js
+++ client/src/hooks/useLocalStorage.js
@@ -2,17 +2,21 @@ import { useState, useEffect } from "react";
 
 const PREFIX = "swiftchat-";
 
+function getStoredValue(prefixedKey, initialValue) {
+  const jsonValue = localStorage.getItem(prefixedKey);
+
+  if (jsonValue != null) return JSON.parse(jsonValue);
+  if (typeof initialValue === "function") {
+    return initialValue();
+  } else {
+    return initialValue;
+  }
+}
+
 export default function useLocalStorage(key, initialValue) {
   const prefixedKey = PREFIX + key;
   const [value, setValue] = useState(() => {
-    const jsonValue = localStorage.getItem(prefixedKey);
-
-    if (jsonValue != null) return JSON.parse(jsonValue);
-    if (typeof initialValue === "function") {
-      return initialValue();
-    } else {
-      return initialValue;
-    }
+    return getStoredValue(prefixedKey, initialValue);
   });
 
   useEffect(() => {
