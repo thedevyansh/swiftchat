b3997b28a13c337c6c34ac455e2774f375e32828
thedevyansh <schawla333333@gmail.com>
2021-04-29 15:34:37 +0530

    Show Modals on button click

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,14 +1,21 @@
 import React, { useState } from "react";
-import { Tab, Nav, Button } from "react-bootstrap";
+import { Tab, Nav, Button, Modal } from "react-bootstrap";
 import Contacts from "./Contacts";
 import Conversations from "./Conversations";
+import NewConversationModal from "./NewConversationModal";
+import NewContactModal from "./NewContactModal";
 
 const CONVERSATIONS_KEY = "conversations";
 const CONTACTS_KEY = "contacts";
 
 export default function Sidebar({ id }) {
   const [activeKey, setActiveKey] = useState(CONVERSATIONS_KEY);
+  const [modalOpen, setModalOpen] = useState(false);
   const conversationsOpen = activeKey === CONVERSATIONS_KEY;
+
+  function closeModal() {
+    setModalOpen(false);
+  }
 
   return (
     <div style={{ width: "250px" }} className="d-flex flex-column">
@@ -32,10 +39,18 @@ export default function Sidebar({ id }) {
         <div className="p-2 border-top border-right small">
           Your ID: <span className="text-muted">{id}</span>
         </div>
-        <Button className="rounded-0">
-            New {conversationsOpen ? "Conversations" : "Contacts"}
+        <Button onClick={() => setModalOpen(true)} className="rounded-0">
+          New {conversationsOpen ? "Conversations" : "Contacts"}
         </Button>
       </Tab.Container>
+
+      <Modal show={modalOpen} onHide={closeModal}>
+        {conversationsOpen ? (
+          <NewConversationModal closeModal={closeModal} />
+        ) : (
+          <NewContactModal closeModal={closeModal} />
+        )}
+      </Modal>
     </div>
   );
 }
