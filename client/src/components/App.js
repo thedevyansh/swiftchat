09912070484fedc133197bd7a65b972835171a71
thedevyansh <schawla333333@gmail.com>
2021-04-29 15:33:50 +0530

    Added: Context Provider for dashboard

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,13 +2,21 @@ import React from "react";
 import Login from "./Login";
 import useLocalStorage from "../hooks/useLocalStorage";
 import Dashboard from "./Dashboard";
+import { ContactsProvider } from "../contexts/ContactsProvider";
+import { ConversationsProvider } from "../contexts/ConversationsProvider";
 
 function App() {
   const [id, setId] = useLocalStorage("id", "");
 
-  return (
-   id ? <Dashboard id={id} /> : <Login onIdSubmit={setId} />
+  const dashboard = (
+    <ContactsProvider>
+      <ConversationsProvider>
+        <Dashboard id={id} />
+      </ConversationsProvider>
+    </ContactsProvider>
   );
+
+  return id ? dashboard : <Login onIdSubmit={setId} />;
 }
 
 export default App;
