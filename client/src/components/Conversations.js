37ce0ae6daf85b19633d53c3adc5c11198b3f7e3
thedevyansh <schawla333333@gmail.com>
2021-04-30 19:03:31 +0530

    shows recipients in conversation tab

--- client/src/components/Conversations.js
+++ client/src/components/Conversations.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import { ListGroup } from "react-bootstrap";
+import { useConversations } from "../contexts/ConversationsProvider";
 
 export default function Conversations() {
-    return (
-        <div>
-            Conversations
-        </div>
-    )
+  const { conversations, selectConversationIndex } = useConversations();
+
+  return (
+    <ListGroup variant="flush">
+      {conversations.map((conversation, index) => (
+        <ListGroup.Item
+          key={index}
+          action
+          onClick={() => selectConversationIndex(index)}
+          active={conversation.selected}
+        >
+          {conversation.recipients
+            .map((recipient) => recipient.name)
+            .join(", ")}
+        </ListGroup.Item>
+      ))}
+    </ListGroup>
+  );
 }
