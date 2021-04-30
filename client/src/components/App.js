4236f3006812aeef5f7364c5a22763604313dde3
thedevyansh <schawla333333@gmail.com>
2021-04-30 19:01:01 +0530

    Passes id to conversation provider

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -10,7 +10,7 @@ function App() {
 
   const dashboard = (
     <ContactsProvider>
-      <ConversationsProvider>
+      <ConversationsProvider id={id}>
         <Dashboard id={id} />
       </ConversationsProvider>
     </ContactsProvider>
