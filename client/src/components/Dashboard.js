989aa46b0490f448477a56ea77165b5fa8a2d0c5
thedevyansh <schawla333333@gmail.com>
2021-04-30 19:02:32 +0530

    shows chat section based on selected contacts

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { useConversations } from "../contexts/ConversationsProvider";
+import OpenConversation from "./OpenConversation";
 import Sidebar from "./Sidebar";
 
 export default function Dashboard({ id }) {
+  const { selectedConversation } = useConversations();
   return (
-     <div className="d-flex" style={{height:"100vh"}}>
-          <Sidebar id={id} />
-     </div>
-    );
+    <div className="d-flex" style={{ height: "100vh" }}>
+      <Sidebar id={id} />
+      {selectedConversation && <OpenConversation />}
+    </div>
+  );
 }
