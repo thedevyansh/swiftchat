092eacadc5a306e8e8b14a221d165c6850355a6a
thedevyansh <schawla333333@gmail.com>
2021-05-01 15:03:12 +0530

    Added: client side logic for sending messages using socket.io

--- client/src/contexts/ConversationsProvider.js
+++ client/src/contexts/ConversationsProvider.js
@@ -1,5 +1,7 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect, useCallback } from "react";
 import useLocalStorage from "../hooks/useLocalStorage";
+import { useContacts } from "./ContactsProvider";
+import { useSocket } from "./SocketProvider";
 
 const ConversationsContext = React.createContext();
 
@@ -7,8 +9,14 @@ export function useConversations() {
   return useContext(ConversationsContext);
 }
 
-export function ConversationsProvider({ children }) {
-  const [conversations, setConversations] = useLocalStorage("conversations", []);
+export function ConversationsProvider({ id, children }) {
+  const [conversations, setConversations] = useLocalStorage(
+    "conversations",
+    []
+  );
+  const [selectedConversationIndex, setSelectedConversationIndex] = useState(0);
+  const { contacts } = useContacts();
+  const socket = useSocket();
 
   function createConversation(recipients) {
     setConversations((prevConversations) => {
@@ -16,11 +24,93 @@ export function ConversationsProvider({ children }) {
     });
   }
 
-  return (
-    <div>
-      <ConversationsContext.Provider value={{ conversations, createConversation }}>
-        {children}
-      </ConversationsContext.Provider>
-    </div>
+  const addMessageToConversation = useCallback(
+    ({ recipients, text, sender }) => {
+      setConversations((prevConversations) => {
+        let madeChange = false;
+        const newMessage = { sender, text };
+        const newConversations = prevConversations.map((conversation) => {
+          if (arrayEquality(conversation.recipients, recipients)) {
+            madeChange = true;
+            return {
+              ...conversation,
+              messages: [...conversation.messages, newMessage],
+            };
+          }
+
+          return conversation;
+        });
+
+        if (madeChange) {
+          return newConversations;
+        } else {
+          return [...prevConversations, { recipients, messages: [newMessage] }];
+        }
+      });
+    },
+    [setConversations]
   );
+
+  useEffect(() => {
+    if (socket == null) return;
+    socket.on("receive-message", addMessageToConversation);
+
+    return () => socket.off("receive-message");
+  }, [socket, addMessageToConversation]);
+
+  function sendMessage(recipients, text) {
+    socket.emit("send-message", { recipients, text });
+    addMessageToConversation({ recipients, text, sender: id });
+  }
+
+  const formattedConversations = conversations.map((conversation, index) => {
+    const recipients = conversation.recipients.map((recipient) => {
+      const contact = contacts.find((contact) => {
+        return contact.id === recipient;
+      });
+
+      const name = (contact && contact.name) || recipient;
+
+      return { id: recipient, name };
+    });
+
+    const messages = conversation.messages.map((message) => {
+      const contact = contacts.find((contact) => {
+        return contact.id === message.sender;
+      });
+
+      const name = (contact && contact.name) || message.sender;
+      const fromMe = id === message.sender;
+
+      return { ...message, senderName: name, fromMe };
+    });
+
+    const selected = index === selectedConversationIndex;
+    return { ...conversation, messages, recipients, selected };
+  });
+
+  const value = {
+    conversations: formattedConversations,
+    createConversation,
+    selectConversationIndex: setSelectedConversationIndex,
+    selectedConversation: formattedConversations[selectedConversationIndex],
+    sendMessage,
+  };
+
+  return (
+    <ConversationsContext.Provider value={value}>
+      {children}
+    </ConversationsContext.Provider>
+  );
+}
+
+function arrayEquality(a, b) {
+  if (a.length !== b.length) return false;
+
+  a.sort();
+  b.sort();
+
+  return a.every((element, index) => {
+    return element === b[index];
+  });
 }
